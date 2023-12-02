e94d3e82b7202664ed74eab0bd5139b28059e8f5
Trevor Chan <tjchan0@gmail.com>
2023-12-02 10:21:56 -0500

    initial commit

---
+++ pose_extractor/build/tmp/CMakeFiles/pose_extractor.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/trevorchan/repos/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/src/extract_poses.cpp" "/Users/trevorchan/repos/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/build/tmp/CMakeFiles/pose_extractor.dir/src/extract_poses.cpp.o"
+  "/Users/trevorchan/repos/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/src/human_pose.cpp" "/Users/trevorchan/repos/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/build/tmp/CMakeFiles/pose_extractor.dir/src/human_pose.cpp.o"
+  "/Users/trevorchan/repos/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/src/peak.cpp" "/Users/trevorchan/repos/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/build/tmp/CMakeFiles/pose_extractor.dir/src/peak.cpp.o"
+  "/Users/trevorchan/repos/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/wrapper.cpp" "/Users/trevorchan/repos/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/build/tmp/CMakeFiles/pose_extractor.dir/wrapper.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "pose_extractor_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../src"
+  "/usr/local/Frameworks/Python.framework/Versions/3.7/include/python3.7m"
+  "/Users/trevorchan/environments/rehab/lib/python3.7/site-packages/numpy/core/include"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ pose_extractor/build/tmp/CMakeFiles/pose_extractor.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../pose_extractor.pdb"
+  "../pose_extractor.so"
+  "CMakeFiles/pose_extractor.dir/src/extract_poses.cpp.o"
+  "CMakeFiles/pose_extractor.dir/src/human_pose.cpp.o"
+  "CMakeFiles/pose_extractor.dir/src/peak.cpp.o"
+  "CMakeFiles/pose_extractor.dir/wrapper.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pose_extractor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
